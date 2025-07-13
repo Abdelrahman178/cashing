b625c40854190367899ed47041d945c5f31c592c
Abdelrahman_Amer <bedoamer33352@gmail.com>
2025-07-13 05:59:58 +0300

    ✅ Implemented user data saving using UserDefaults

--- cashing/ViewController.swift
+++ cashing/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var ageField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadUser()
+
+    }
+    
+    func getUser() -> User {
+        let name = nameField.text ?? ""
+        let age = Int(ageField.text ?? "") ?? 0
+        return User(name: name, age: age)
+    }
+    
+    func loadUser() {
+        let name = UserDefaults.standard.string(forKey: "name") ?? ""
+        let age = UserDefaults.standard.integer(forKey: "age")
+        nameField.text = name
+        ageField.text = "\(age)"
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+        let user = getUser()
+        
+        saveUser(user)
+    }
+    
+    func saveUser(_ user: User) {
+        UserDefaults.standard.set(user.name, forKey: "name")
+        UserDefaults.standard.set(user.age, forKey: "age")
+        print("✅ Saved to UserDefaults")
     }
 
-
+    
+    // Basic struct for passing user data
+    struct User {
+        var name: String
+        var age: Int
+    }
 }
-
