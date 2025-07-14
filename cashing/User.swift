5636fea74451f5a2c120480d284f8dfdc9c200ce
Abdelrahman_Amer <bedoamer33352@gmail.com>
2025-07-14 14:33:29 +0300

    realm

---
+++ cashing/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  cashing
+//
+//  Created by Abdelrahman Amer on 14/07/2025.
+//
+
+import Foundation
+
+
+class User: Object {
+    @objc dynamic var name: String = ""
+    @objc dynamic var age: Int = 0
+}
+
--- cashing/ViewController.swift
+++ cashing/ViewController.swift
@@ -4,42 +4,40 @@
 //
 //  Created by Abdelrahman Amer on 10/07/2025.
 //
-
 import UIKit
 
+import RealmSwift
+
 class ViewController: UIViewController {
-    
-    
+
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var ageField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
     func getUser() -> User {
-        let name = nameField.text ?? ""
-        let age = Int(ageField.text ?? "") ?? 0
-        return User(name: name, age: age)
+        let user = User()
+        user.name = nameField.text ?? ""
+        user.age = Int(ageField.text ?? "") ?? 0
+        return user
     }
-    
 
-    
     @IBAction func saveButtonTapped(_ sender: UIButton) {
         let user = getUser()
-        
         saveUser(user)
     }
     
     func saveUser(_ user: User) {
-        //not implemented
-    }
-
-    
-    // Basic struct for passing user data
-    struct User {
-        var name: String
-        var age: Int
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.add(user)
+            }
+            print("User saved to Realm.")
+        } catch {
+            print("Error saving user: \(error)")
+        }
     }
 }
